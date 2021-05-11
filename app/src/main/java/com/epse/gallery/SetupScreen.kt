6730c76aa976986d31dd37b5aba037dc3ec896d3
riccardoforzan <forzanric@gmail.com>
2021-05-11 13:56:24 +0200

    Adding elements to SetupScreen.kt Added some elements and a reference to a library to take inspiration from

--- app/src/main/java/com/epse/gallery/SetupScreen.kt
+++ app/src/main/java/com/epse/gallery/SetupScreen.kt
@@ -1,34 +1,63 @@
 package com.epse.gallery
 
+import android.net.Uri
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.coil.rememberCoilPainter
 
 /**
  * This composable UI is shown when the app is started but the permissions are not granted to it.
  * The purpose of this UI is asking for permissions respecting the policy of Android.
  * https://developer.android.com/training/permissions/requesting
  */
-class SetupScreen {
 
+class SetupScreen() {
+
+    /**
+     * Take inspiration from
+     * https://github.com/AppIntro/AppIntro
+     */
     @Composable
     fun askForReadStorage(){
-        MaterialTheme {
-            Column(
-                modifier = Modifier.background(Color.Green)
-            ) {
-                Text(
-                    text = "Hello theming",
-                    color = MaterialTheme.colors.primary
+
+        Image(
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = rememberCoilPainter(
+                request = Uri.parse("content://media/external/images/media/31")
+            )
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = MaterialTheme.colors.background,
                 )
+            ) {
+
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterStart),
+                    text = "Descrizione dell'applicazione")
+
+            TextButton(
+                onClick = {},
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+            ) {
+                Text("Dai i permessi")
             }
         }
-
     }
 
     @Preview(showBackground = true)
