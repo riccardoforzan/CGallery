24f4b6ffed865b2d985f1d360be924366cb66ed3
riccardoforzan <forzanric@gmail.com>
2021-05-18 11:50:08 +0200

    Reorganized permisisons UIs in PermissionScreen.kt

--- app/src/main/java/com/epse/gallery/MainActivity.kt
+++ app/src/main/java/com/epse/gallery/MainActivity.kt
@@ -5,38 +5,25 @@ import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
-import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
 import com.epse.gallery.screen.*
-import com.google.accompanist.coil.rememberCoilPainter
 
 @ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
 
-    private val storagePermissionCode = 1
-
     companion object{
+        val storagePermissionCode = 1
         var isPortrait by mutableStateOf(true)
     }
 
@@ -48,6 +35,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    /**
+     * TODO: Improve, when app is in background and permission changes from denied to granted
+     */
     override fun onResume() {
         if(StorageUtils.hasReadStoragePermission(this)){
             super.onResume()
@@ -61,7 +51,7 @@ class MainActivity : ComponentActivity() {
      * https://developer.android.com/training/permissions/requesting
      */
     @SuppressLint("MissingSuperCall")
-    override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<out String>,
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
                                              grantResults: IntArray) {
 
         if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
@@ -70,7 +60,7 @@ class MainActivity : ComponentActivity() {
             }
         } else {
             setContent{
-                ReadStorageDenied()
+                PermissionScreen(this).ReadStorageDenied()
             }
         }
 
@@ -89,81 +79,12 @@ class MainActivity : ComponentActivity() {
                 val shouldShowRationaleUI = this.shouldShowRequestPermissionRationale(permission)
                 Log.d("DEB SHOW RATIONALE", shouldShowRationaleUI.toString())
                 if(shouldShowRationaleUI){
-                    RationateUI()
+                    PermissionScreen(this).RationateUI()
                 } else {
-                    ReadStorageDenied()
+                    PermissionScreen(this).ReadStorageDenied()
                 }
             } else {
-                ReadStorageDenied()
-            }
-        }
-    }
-
-    @Composable
-    fun RationateUI(){
-        MaterialTheme {
-            val typography = MaterialTheme.typography
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    /**
-                     * TODO:
-                     * Insert application logo as request parameter
-                     */
-                    painter = rememberCoilPainter(request = null),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(120.dp)
-                        .fillMaxWidth()
-                        .clip(shape = RoundedCornerShape(4.dp)),
-                    contentScale = ContentScale.Crop
-                )
-                Spacer(Modifier.height(16.dp))
-
-                Text(
-                    text=this@MainActivity.getString(R.string.permission_read_external_storage_description),
-                    style = typography.h6
-                )
-
-                Button(
-                    modifier = Modifier.padding(30.dp),
-                    onClick = {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            val permission = android.Manifest.permission.READ_EXTERNAL_STORAGE
-                            this@MainActivity.requestPermissions(arrayOf(permission),storagePermissionCode)
-                        }
-                    }
-                ){
-                    Text(
-                        text = this@MainActivity.getString(R.string.btn_manage_permissions),
-                    )
-                }
-
-            }
-        }
-
-    }
-
-    /**
-     * Screen to show when read on external storage permission has not been granted
-     */
-    @Composable
-    fun ReadStorageDenied(){
-        MaterialTheme{
-            val typography = MaterialTheme.typography
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text=this@MainActivity.getString(R.string.permission_read_external_storage_not_granted),
-                    style = typography.h6
-                )
+                PermissionScreen(this).ReadStorageDenied()
             }
         }
     }
