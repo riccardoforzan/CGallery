83d2bc7c176a80fc4c64c40448aaef02fb13c0ec
RiccardoTumiati <richifenomeno@gmail.com>
2021-05-26 20:38:21 +0200

    Update of fullImage. Scroll image system updated

--- app/src/main/java/com/epse/gallery/screen/FullImage.kt
+++ app/src/main/java/com/epse/gallery/screen/FullImage.kt
@@ -2,13 +2,11 @@ package com.epse.gallery.screen
 
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.*
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -26,14 +24,13 @@ import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
-import com.epse.gallery.MainActivity
 import com.epse.gallery.StorageUtils
 import com.google.accompanist.coil.rememberCoilPainter
 import kotlin.math.roundToInt
 
 @ExperimentalFoundationApi
 class FullImage(private val ctx: Context, private val navController: NavHostController) {
-
+/*
     private var backgroundColor = Color.Black
     private lateinit var myURI: Uri
     private var showButton by mutableStateOf(false)
@@ -44,7 +41,7 @@ class FullImage(private val ctx: Context, private val navController: NavHostCont
 
     @Composable
     fun ShowFullImage(imageURI: Uri) {
-        myURI = Uri.parse(imageURI.toString())
+        myURI = imageURI
         val paint = rememberCoilPainter(myURI)
         Box(modifier = Modifier
             .fillMaxSize()
@@ -147,6 +144,166 @@ class FullImage(private val ctx: Context, private val navController: NavHostCont
             if (state) 90f else 0f
         }
         val scale: Dp by transition.animateDp() { state ->
+            if (state) 40.dp else 0.dp
+        }
+        FloatingActionButton(
+            backgroundColor = Color.White,
+            modifier = Modifier
+                .padding(bottom = 10.dp, start = 25.dp)
+                .size(scale),
+            onClick = {
+            }
+        ) {
+            Icon(
+                Icons.Filled.Delete,
+                contentDescription = null
+            )
+        }
+        FloatingActionButton(
+            backgroundColor = Color.White,
+            modifier = Modifier
+                .padding(bottom = 10.dp, start = 25.dp)
+                .size(scale),
+            onClick = {
+                navController.navigate(
+                    route = Screens.ImageDetails_ShowDetail + "/${myURI}"
+                )
+            }
+        ) {
+            Icon(
+                Icons.Filled.Info,
+                contentDescription = null
+            )
+        }
+        FloatingActionButton(
+            backgroundColor = Color.White,
+            modifier = Modifier
+                .padding(10.dp),
+            onClick = {
+                expandedState = !expandedState
+            }
+        ) {
+            Icon(
+                Icons.Filled.List,
+                contentDescription = null,
+                modifier = Modifier
+                    .rotate(rotation)
+            )
+        }
+    }*/
+    private var backgroundColor = Color.Black
+    private var myURI by mutableStateOf("")
+    private var lastChange=System.currentTimeMillis()
+    private var showButton by mutableStateOf(false)
+    private var expandedState by mutableStateOf(false)
+    private var zoom by mutableStateOf(1f)
+    private var offsetX by mutableStateOf(0f)
+    private var offsetY by mutableStateOf(0f)
+    private var images=StorageUtils.getImageURIs()
+
+    @Composable
+    fun ShowFullImage(imageURI: Uri) {
+        myURI=imageURI.toString()
+        BuildFullImage()
+    }
+
+    @Composable
+    private fun BuildFullImage() {
+        var uri = Uri.parse(myURI)
+        val paint = rememberCoilPainter(uri)
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = {
+                        if (zoom == 1f)
+                            zoom = 2.5f
+                        else {
+                            zoom = 1f
+                            offsetX = 0f
+                            offsetY = 0f
+                        }
+                    },
+                    onTap = { showButton = !showButton }
+                )
+            }
+        ) {
+            Image(painter = paint, contentDescription = null, modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .align(Alignment.Center)
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .graphicsLayer(
+                    scaleX = zoom,
+                    scaleY = zoom
+                )
+                .pointerInput(Unit) {
+                    detectTransformGestures(
+                        onGesture = { _, pan, gestureZoom, _ ->
+                            val newZoom = zoom * gestureZoom
+                            zoom = maxOf(1.0F, minOf(3.0F, newZoom))
+                            val currentTime=System.currentTimeMillis()
+                            if(zoom==1f){
+                                if (pan.x >= 50 && currentTime-lastChange>200) {
+                                    uri = Uri.parse(myURI)
+                                    val index= images.indexOf(uri)
+                                    var newIndex = index-1
+                                    if (newIndex <= -1)
+                                        newIndex = 0
+                                    lastChange=System.currentTimeMillis()
+                                    myURI=images[newIndex].toString()
+                                }
+                                if (pan.x <= -50 && currentTime-lastChange>200) {
+                                    uri = Uri.parse(myURI)
+                                    val index= images.indexOf(uri)
+                                    var newIndex = index+1
+                                    if (newIndex >= images.size)
+                                        newIndex = images.size - 1
+                                    lastChange=System.currentTimeMillis()
+                                    myURI=images[newIndex].toString()
+                                }
+                            }else{
+                                val newOffSetX = offsetX + pan.x * zoom
+                                val maxOffSetX = (paint.intrinsicSize.width / 2) * (zoom - 1)
+                                offsetX = maxOf(maxOffSetX * (-1), minOf(maxOffSetX, newOffSetX))
+
+                                val newOffSetY = offsetY + pan.y * zoom
+                                val maxOffSetY = (paint.intrinsicSize.height / 2) * (zoom - 1)
+                                offsetY = maxOf(maxOffSetY * (-1), minOf(maxOffSetY, newOffSetY))
+                            }
+                            showButton = false
+                            expandedState = false
+                        }
+                    )
+                })
+
+            if (showButton) {
+                Icon(Icons.Filled.ArrowBack,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(30.dp)
+                        .align(Alignment.TopStart)
+                        .clickable {
+                            navController.navigate(route = Screens.ImagesGrid_ShowGrid)
+                        }
+                )
+                Column(modifier=Modifier.align(Alignment.BottomEnd)){
+                    MultiActionButton()
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun MultiActionButton() {
+        val transition = updateTransition(targetState = expandedState)
+        val rotation: Float by transition.animateFloat { state ->
+            if (state) 90f else 0f
+        }
+        val scale: Dp by transition.animateDp { state ->
             if (state) 40.dp else 0.dp
         }
         FloatingActionButton(
