d29be3a7f9476d89d34d06b29f4006b54017f52b
riccardoforzan <forzanric@gmail.com>
2021-06-06 09:49:45 +0200

    Changes the name of the gallery

--- app/src/main/java/com/epse/gallery/screen/Settings.kt
+++ app/src/main/java/com/epse/gallery/screen/Settings.kt
@@ -13,14 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.epse.gallery.FirstTimeActivity
 import com.epse.gallery.SPUtils
 import com.epse.gallery.ui.theme.GalleryTheme
 
 class Settings(private val ctx: Context, private val navController: NavHostController) {
 
     //Contains the name of the gallery
-    private var textState = mutableStateOf(TextFieldValue())
+    var textState = mutableStateOf(TextFieldValue(""))
 
     @Composable
     fun ShowSettings() {
@@ -32,12 +31,6 @@ class Settings(private val ctx: Context, private val navController: NavHostContr
                         navigationIcon = {
                             IconButton(onClick = {
                                 navController.popBackStack()
-                                val sp = ctx.getSharedPreferences(SPUtils.preferences, Context.MODE_PRIVATE)
-                                with(sp.edit()) {
-                                    //Save the text value
-                                    putString(SPUtils.gallery_title, textState.value.text)
-                                    apply()
-                                }
                             }) {
                                 Icon(Icons.Filled.ArrowBack, contentDescription = null)
                             }
@@ -55,21 +48,44 @@ class Settings(private val ctx: Context, private val navController: NavHostContr
 
                     //Set a placeholder with the value saved on shared preferences
                     val sp = ctx.getSharedPreferences(SPUtils.preferences, Context.MODE_PRIVATE)
-                    val initvalue = sp.getString(SPUtils.gallery_title,"")!!
+                    val actual = sp.getString(SPUtils.gallery_title,"")!!
+                    Text(text="Actual name: $actual")
 
                     OutlinedTextField(
                         value = textState.value,
                         onValueChange = {
                             textState.value = it
+
                         },
                         label = {Text("Set your gallery name")},
-                        placeholder = {Text(initvalue)}
                     )
 
                     Text("New gallery name: " + textState.value.text)
+
+                    Button(
+                        colors = ButtonDefaults.textButtonColors(
+                            backgroundColor = MaterialTheme.colors.primary,
+                            contentColor = MaterialTheme.colors.onPrimary
+                        ),
+                        onClick = {
+                            //Update on shared preferences
+                            val sp = ctx.getSharedPreferences(SPUtils.preferences, Context.MODE_PRIVATE)
+                            with(sp.edit()) {
+                                //Save the text value
+                                putString(SPUtils.gallery_title, textState.value.text)
+                                apply()
+                            }
+                        }
+                    ){
+                        Text(
+                            text = "Change name"
+                        )
+                    }
                 }
+
             }
         }
     }
+
 
 }
